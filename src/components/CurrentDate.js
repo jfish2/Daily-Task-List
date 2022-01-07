26ea959e2c36d96dafc70f4ce6637fc5373859f9
Jonny Fisher <jonathan@trueanthem.com>
2022-01-07 15:08:07 -0800

    Update CurrentDate.js

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function CurrentDate() {
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    const weeks = ["Sunday", "Monday", "Tuesday","Thursday", "Friday", "Saturday"]
+    const weeks = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
     const date = new Date();
     const month =  months[date.getMonth()]
     const year = date.getFullYear()
